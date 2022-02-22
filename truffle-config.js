d4d487dcab1788fa86821e5303a6f64b1cf850db
gykim <gybryce@gmail.com>
2022-02-22 09:02:04 +0900

    main net add

--- truffle-config.js
+++ truffle-config.js
@@ -2,7 +2,9 @@ const HDWalletProvider = require("truffle-hdwallet-provider-klaytn");
 const NETWORK_ID = '1001'
 const GASLIMIT = '9000000'
 const URL = `https://api.baobab.klaytn.net:8651`
-const PRIVATE_KEY = '0xfd4bfae0230261f642b2c432ecef138bb772825575c76545532520792b827ec0'
+// baobab
+const PRIVATE_KEY_testnet = '0xfd4bfae0230261f642b2c432ecef138bb772825575c76545532520792b827ec0'
+const PRIVATE_KEY_mainnet = '0xc11d77744b5be9aae6dbaf7ad657ae78f82ebddd2b595ba29a8c3df521684f03'
 
 module.exports = {
   networks: {  
@@ -13,10 +15,17 @@ module.exports = {
     },
 
     klaytn: {
-      provider: new HDWalletProvider(PRIVATE_KEY, URL),
+      provider: new HDWalletProvider(PRIVATE_KEY_testnet, URL),
       network_id: NETWORK_ID,
       gas: GASLIMIT,
       gasPrice: null,
-    }  
+    },
+
+    mainnet: {
+      provider: () => new HDWalletProvider(PRIVATE_KEY_mainnet, "https://public-node-api.klaytnapi.com/v1/cypress"),
+      network_id: '8217', //Klaytn mainnet's network id
+      gas: '1000000',
+      gasPrice: null
+    }
   }
 }
